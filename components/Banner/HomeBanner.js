abb3e282f968a28ba0eb5de7c7c5f2384e4c90d3
T31K <t31kmunwong@gmail.com>
2023-10-15 16:47:28 +0800

    add ph badge

--- components/Banner/HomeBanner.js
+++ components/Banner/HomeBanner.js
@@ -13,7 +13,18 @@ export const HomeBanner = () => {
             <SectionContainer className="page-banner--inner-container wrap wrap-px z-10">
                 {/* Appear First */}
                 <MotionBTTContainer transition={{ delay: 0.2, duration: 0.5 }}>
-                    {/* add PH badge here */}
+                    <a
+                        href="https://www.producthunt.com/posts/sidepod?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-sidepod"
+                        target="_blank"
+                    >
+                        <img
+                            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=417879&theme=dark"
+                            alt="Sidepod - A&#0032;Spotify&#0032;widget&#0032;to&#0032;control&#0032;your&#0032;music&#0032;from&#0032;anywhere | Product Hunt"
+                            style="width: 250px; height: 54px;"
+                            width="250"
+                            height="54"
+                        />
+                    </a>
                     <BadgeGroup alignment="center" className={"hidden"}>
                         <BadgeMessage>Discover NutriTrack!</BadgeMessage>
                         <BadgeIcon icon="game-icons:meal" />
