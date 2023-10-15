575688acb3e9155bfded8ca329da2906595994d3
T31K <t31kmunwong@gmail.com>
2023-10-15 16:49:54 +0800

    fix

--- components/Banner/HomeBanner.js
+++ components/Banner/HomeBanner.js
@@ -13,16 +13,13 @@ export const HomeBanner = () => {
             <SectionContainer className="page-banner--inner-container wrap wrap-px z-10">
                 {/* Appear First */}
                 <MotionBTTContainer transition={{ delay: 0.2, duration: 0.5 }}>
-                    <a
-                        href="https://www.producthunt.com/posts/sidepod?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-sidepod"
-                        target="_blank"
-                    >
+                    <a href="https://www.producthunt.com/posts/sidepod?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-sidepod">
                         <img
                             src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=417879&theme=dark"
                             alt="Sidepod - A&#0032;Spotify&#0032;widget&#0032;to&#0032;control&#0032;your&#0032;music&#0032;from&#0032;anywhere | Product Hunt"
-                            style="width: 250px; height: 54px;"
                             width="250"
                             height="54"
+                            className="w-[250px] h-[54px]"
                         />
                     </a>
                     <BadgeGroup alignment="center" className={"hidden"}>
